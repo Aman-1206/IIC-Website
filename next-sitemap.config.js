6d9c6cbd3471e8e7f371c2caaa09c25f0bdce272
icouncil-slc <icouncil@shyamlal.du.ac.in>
2025-11-07 16:46:11 +0530

    Added sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next-sitemap').IConfig} */
+const siteUrl = 'https://iic-slc.live'; // 🧠 change this
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true, // ✅ auto-generate robots.txt
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/admin/*', '/api/*','/manage-team','/add-webinar','/add-sponsor','/add-past-events','/add-gallery','/add-event','/add-collaborate'], 
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [
+      `${siteUrl}/sitemap.xml`,
+    ],
+  },
+};
